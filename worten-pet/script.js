73d0f8aee124f8163d332d4e8de68ace2bbc61da
unknown <krevilraj@gmail.com>
2021-11-16 21:18:33 +0545

    script changed

--- worten-pet/script.js
+++ worten-pet/script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    alert('test')
+    // alert('test')
+    
+    $('.pet__row__item a').click(function(e){
+        e.preventDefault();
+        var pr__class = $(this).data('pr');
+        $('.pet__row__item__info').removeClass('active');
+        $('.'+pr__class).addClass('active');
+    });
+
+
 });
