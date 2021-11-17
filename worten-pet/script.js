0d4a547b44e8aa9ed64c21511fc64f40d4459539
unknown <krevilraj@gmail.com>
2021-11-17 15:21:41 +0545

    version1

--- worten-pet/script.js
+++ worten-pet/script.js
@@ -3,38 +3,82 @@ $(document).ready(function () {
 
   $('.pet__row__item').click(function (e) {
     e.preventDefault();
-    if($(this).hasClass('active')){
-      var parent_class = $(this).closest('.pet__row__inner');
-      parent_class.find('.pet__row__item').removeClass('deactivate');
+    var windowsize = $(window).width();
+    var pr__class = $(this).data('pr');
+    var parent_class = $(this).closest('.pet__row__inner');
+    if ($(this).hasClass('active')) {
+
       parent_class.find('.pet__row__item').removeClass('active');
-      $('.pet__row__item__info').removeClass('active');
-    }else{
-      var pr__class = $(this).data('pr');
-      var parent_class = $(this).closest('.pet__row__inner');
-      // parent_class.find('.pet__row__item').addClass('deactivate');
+      if (windowsize > 600) {
+        pet__animation(parent_class, pr__class, "0", "0", false);
+      }else{
+        parent_class.find('.' + pr__class).slideUp('slow')
+          .animate(
+            { opacity: 1 },
+            { queue: false, duration: 'slow' }
+          );
+      }
+
+    } else {
+
       $(this).addClass('active');
       $(this).removeClass('deactivate');
       $('.pet__row__item__info').removeClass('active');
-      // parent_class.find('.' + pr__class).addClass('active');
 
-      if(pr__class == "pr__1"){
-        parent_class.find('.' + pr__class).delay(800).fadeIn(500);
-        parent_class.find('[data-pr="pr__2"]').css({"transform":"translate(-400px,0)","z-index":"1"}).delay(800).fadeOut(0);
-        parent_class.find('[data-pr="pr__3"]').css({"transform":"translate(-800px,0)","z-index":"2"}).delay(800).fadeOut(0);
-        parent_class.find('[data-pr="pr__1"]').css({"z-index":"3"});
-
-      } else if(pr__class == "pr__2"){
-
+      if (windowsize > 1131) {
+        pet__animation(parent_class, pr__class, "-400px", "-800px", true);
+      } else if (windowsize < 1131 && windowsize > 1072) {
+        pet__animation(parent_class, pr__class, "-367px", "-743px", true);
+      } else if (windowsize < 1072 && windowsize > 991) {
+        pet__animation(parent_class, pr__class, "-333px", "-682px", true);
+      } else if (windowsize < 991 && windowsize > 888) {
+        pet__animation(parent_class, pr__class, "-313px", "-645px", true);
+      }else if (windowsize < 888 && windowsize > 808) {
+        pet__animation(parent_class, pr__class, "-288px", "-593px", true);
+      }else if (windowsize < 808 && windowsize > 750) {
+        pet__animation(parent_class, pr__class, "-260px", "-544px", true);
+      }else if (windowsize < 750 && windowsize > 706) {
+        pet__animation(parent_class, pr__class, "-250px", "-507px", true);
+      }else if (windowsize < 706 && windowsize > 648) {
+        pet__animation(parent_class, pr__class, "-240px", "-470px", true);
+      }else if (windowsize < 648 && windowsize > 600) {
+        pet__animation(parent_class, pr__class, "-230px", "-419px", true);
+      }else{
+        parent_class.find('.' + pr__class).slideDown('slow')
+          .animate(
+            { opacity: 1 },
+            { queue: false, duration: 'slow' }
+          );
       }
-      else if(pr__class == "pr__3"){
 
-      }
     }
 
   });
 
+  function pet__animation(parent_class, pr__class, second_px, third_px, fadeIn) {
+    if (fadeIn) {
+      parent_class.find('.' + pr__class).fadeIn(600);
+    } else {
+      parent_class.find('.' + pr__class).fadeOut(1000);
+    }
+    if (pr__class == "pr__1") {
+      parent_class.find('[data-pr="pr__2"]').css({"transform": "translate(" + second_px + ",0)", "z-index": "1"});
+      parent_class.find('[data-pr="pr__3"]').css({"transform": "translate(" + third_px + ",0)", "z-index": "2"});
+      parent_class.find('[data-pr="pr__1"]').css({"z-index": "3"});
 
-  $('.pet__dogcat__nav ul li a').click(function(e){
+    } else if (pr__class == "pr__2") {
+      parent_class.find('[data-pr="pr__1"]').css({"z-index": "1"});
+      parent_class.find('[data-pr="pr__3"]').css({"transform": "translate(" + third_px + ",0)", "z-index": "2"});
+      parent_class.find('[data-pr="pr__2"]').css({"transform": "translate(" + second_px + ",0)", "z-index": "3"});
+    } else if (pr__class == "pr__3") {
+      parent_class.find('[data-pr="pr__1"]').css({"z-index": "1"});
+      parent_class.find('[data-pr="pr__2"]').css({"transform": "translate(" + second_px + ",0)", "z-index": "2"});
+      parent_class.find('[data-pr="pr__3"]').css({"transform": "translate(" + third_px + ",0)", "z-index": "3"});
+    }
+  }
+
+
+  $('.pet__dogcat__nav ul li a').click(function (e) {
     e.preventDefault();
     $(this).closest('ul').find('li').removeClass('active');
     $(this).closest('li').addClass('active');
